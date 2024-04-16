db0d47ec1d51b0874edfd0246aab69694a0dcd07
mhnd3v <nielsen.177@gmail.com>
2024-04-16 19:48:50 +0200

    fix, console.log output changed from localhost to 0.0.0.0

--- server/src/app.ts
+++ server/src/app.ts
@@ -17,7 +17,7 @@ async function startServer() {
   }
 
   app.listen(PORT, "0.0.0.0", () => {
-    console.log(`Server is running at http://localhost:${PORT}`);
+    console.log(`Server is running at http://0.0.0.0:${PORT}`);
   });
 }
 
