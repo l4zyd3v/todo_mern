720bfae54ed10775d4d68f4c5784d7afa896e0ca
mhnd3v <nielsen.177@gmail.com>
2024-05-05 23:21:06 +0200

    react router added

--- todo_mern_client/src/main.tsx
+++ todo_mern_client/src/main.tsx
@@ -14,7 +14,7 @@ if ("serviceWorker" in navigator) {
 ReactDOM.createRoot(document.getElementById("root")!).render(
   // <React.StrictMode>
   <NavToggleContextProvider>
-    <App />,
+    <App />
   </NavToggleContextProvider>,
   // </React.StrictMode>,
 );
