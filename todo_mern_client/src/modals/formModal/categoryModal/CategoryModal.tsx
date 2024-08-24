421c95e457e6dff394538526d77fab46762a90ec
l4zyd3v <nielsen.177@gmail.com>
2024-08-24 16:38:09 +0200

    props added

--- todo_mern_client/src/modals/formModal/categoryModal/CategoryModal.tsx
+++ todo_mern_client/src/modals/formModal/categoryModal/CategoryModal.tsx
@@ -2,14 +2,18 @@ import s from "./categorymodal.module.scss";
 
 import { useContext } from "react";
 import { DataContext } from "../../../context/DataContext";
-
-import useSwiperTasksSlides from "../../../hooks/components/useSwiperTasksSlides";
+import SwiperTasksSlides from "../../../components/SwiperTasksSLides/SwiperTasksSlides";
 
 export default function CategoryModal() {
   const { tasks } = useContext(DataContext);
   return (
     <div className={s.categoryModal}>
       <h1 className={s.categoryModal__heading}>category name</h1>
+      <SwiperTasksSlides
+        s={s}
+        parentComponent={"CategoryModal"}
+        numberOfSlides={10}
+      />
     </div>
   );
 }
