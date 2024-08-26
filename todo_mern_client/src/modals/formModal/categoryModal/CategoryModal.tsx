e65c0b0c9df16d029808e9304f8befea96dc0cc4
l4zyd3v <nielsen.177@gmail.com>
2024-08-26 05:12:53 +0200

    the CategoryModal is more or less working, but not pretty yet, still needs to have an exit button added. And also approriate css adedd as well

--- todo_mern_client/src/modals/formModal/categoryModal/CategoryModal.tsx
+++ todo_mern_client/src/modals/formModal/categoryModal/CategoryModal.tsx
@@ -2,13 +2,37 @@ import s from "./categorymodal.module.scss";
 
 import { useContext } from "react";
 import { DataContext } from "../../../context/DataContext";
+import { ModalVisibilityContext } from "../../../context/ModalVisibilityContext";
 import SwiperTasksSlides from "../../../components/SwiperTasksSLides/SwiperTasksSlides";
+import { categoryUtilsHandler } from "../../../utilsGlobal/utils";
 
 export default function CategoryModal() {
-  const { tasks } = useContext(DataContext);
+  const { selectedCategory, tasks } = useContext(DataContext);
+  const { categoryModalVisibility } = useContext(ModalVisibilityContext);
+  const { getProgressPercentage } = categoryUtilsHandler();
+
+  function getCategoryModalClassName() {
+    return `${s.categoryModal}  ${
+      categoryModalVisibility
+        ? s.categoryModalVisible
+        : categoryModalVisibility === false
+          ? s.categoryModalHide
+          : categoryModalVisibility === null
+            ? s.defaultVisibilityHidden
+            : ""
+    }`;
+  }
+
   return (
-    <div className={s.categoryModal}>
+    <div className={getCategoryModalClassName()}>
       <h1 className={s.categoryModal__heading}>category name</h1>
+      <span
+        style={{
+          backgroundColor: selectedCategory?.color,
+          width: getProgressPercentage(tasks, selectedCategory?._id) + "%",
+        }}
+        className={s.categoryModal__progressBar}
+      ></span>
       <SwiperTasksSlides
         s={s}
         parentComponent={"CategoryModal"}
